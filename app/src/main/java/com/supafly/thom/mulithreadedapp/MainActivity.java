d8385d78a30bfb19ae4deec10fd68ed377df57cb
LuckyGinger <LuckyGing3r@gmail.com>
2015-06-05 00:35:50 -0600

    Added Threads Added Toasts for clear button push Changed White text to Black text on ListView Added list_veiw.xml Added Progress Bar

--- app/src/main/java/com/supafly/thom/mulithreadedapp/MainActivity.java
+++ app/src/main/java/com/supafly/thom/mulithreadedapp/MainActivity.java
@@ -1,50 +1,49 @@
 package com.supafly.thom.mulithreadedapp;
 
-import android.app.ListActivity;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.nio.channels.FileLockInterruptionException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
-
 public class MainActivity extends ActionBarActivity {
+    private static Context context;
     private String filename = "numbers.txt";
     private FileOutputStream outputStream;
     private FileInputStream inputStream;
     // To view the data from the file
     private ListView myListView = null;
     // Will hold the file data
-    private ArrayList<String> results = new ArrayList<>();
+    private List<String> results = new ArrayList<>();
     // Not really sure what this does...
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<String> adapter = null;
+    private ProgressBar progress = null;
+    private int progressBarStatus1;
+    private int progressBarStatus2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.context = MainActivity.this.getApplicationContext();
+        progress = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     @Override
@@ -70,70 +69,105 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void runCreate(View view) {
-        BufferedWriter bufferWriter = null;
-        try {
-            // Create a file with the filename
-            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-            bufferWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-            String numbers = null;
-            for (int i = 1; i < 11; i++) {
-                // convert an int to a string
-                numbers = new Integer(i).toString();
-                // write the numbers to the file
-                bufferWriter.append(numbers);
-                bufferWriter.newLine();
-                // sleep 250 milliseconds
-                Thread.sleep(250);
+        progressBarStatus1 = 0;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BufferedWriter bufferWriter = null;
+                try {
+                    // Create a file with the filename
+                    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                    bufferWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                    String numbers = null;
+                    for (int i = 1; i < 11; i++) {
+                        // write the numbers to the file
+                        bufferWriter.append(String.valueOf(i));
+                        bufferWriter.newLine();
+                        // sleep 250 milliseconds
+                        Thread.sleep(250);
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (progressBarStatus1 < 100) {
+                                    progressBarStatus1 += 10;
+                                }
+                                progress.setProgress(progressBarStatus1);
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        bufferWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bufferWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        }).start();
     }
 
     public void runLoad(View view) {
-        BufferedReader bufferReader = null;
-        try {
-            // Open the file
-            inputStream = openFileInput(filename);
-            bufferReader = new BufferedReader(new InputStreamReader(inputStream));
+        progressBarStatus2 = 0;
+        new Thread(new Runnable() {
+            public void run() {
+                BufferedReader bufferReader = null;
+                try {
+                    // Open the file
+                    inputStream = openFileInput(filename);
+                    bufferReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            // Read line by line
-            String line;
-            while ((line = bufferReader.readLine()) != null) {
-                results.add(line);
-                // Sleep 250 milliseconds
-                Thread.sleep(250);
+                    // Read line by line
+                    String line;
+                    while ((line = bufferReader.readLine()) != null) {
+                        results.add(line);
+                        // Sleep 250 milliseconds
+                        Thread.sleep(250);
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (progressBarStatus2 < 100) {
+                                    progressBarStatus2 += 10;
+                                }
+                                progress.setProgress(progressBarStatus2);
+                            }
+                        });
+                    }
+                    // Send file information to the ListView
+                    adapter = new ArrayAdapter<String>(context, R.layout.list_view,
+                            results);
+                    myListView = (ListView) findViewById(R.id.list);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            myListView.setAdapter(adapter);
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        bufferReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            // Send file information to the ListView
-            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                    results);
-            myListView = (ListView) this.findViewById(R.id.list);
-            myListView.setAdapter(adapter);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bufferReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
+        }).start();
     }
 
     public void runClear(View view) {
-        adapter.clear();
-        myListView.setAdapter(adapter);
+        if (adapter != null) {
+            adapter.clear();
+            myListView.setAdapter(adapter);
+            progress.setProgress(0);
+            String msg = "Load cleared!";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        } else {
+            String errorMsg = "Nothing to clear";
+            Toast.makeText(this, errorMsg, Toast.LENGTH_SHORT).show();
+        }
     }
 
 
